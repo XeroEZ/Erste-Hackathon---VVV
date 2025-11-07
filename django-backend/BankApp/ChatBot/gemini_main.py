69a28c053f73748a1320fda4785ef4343a3033e8
po073dp <patrik.ondocik@student.tuke.sk>
2025-11-07 16:59:33 +0100

    gemini

---
+++ django-backend/BankApp/ChatBot/gemini_main.py
@@ -0,0 +1,40 @@
+from . import geminiKey
+
+
+def AkinatorHra(otazka) -> str:
+
+    try:
+        client = geminiKey.ClientApi()
+        
+
+        prompt_text = ({otazka}
+        )
+
+        config = geminiKey.types.GenerateContentConfig(
+            temperature=0.0
+        )
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=prompt_text,
+            config=config,
+        )
+        return response.text.strip()
+
+    except Exception as e:
+        return f"Nastala chyba pri volaní AI: {e}"
+
+
+
+def main():
+    print("Vitaj v hre Akinator!")
+    # spýtaj sa používateľa na odpoveď
+    user_input = input("Zadaj svoju odpoveď (napr. áno/nie): ")
+    # volanie tvojej funkcie AkinatorHra s parametrom
+    result = AkinatorHra(user_input)
+    # vypíš výsledok
+    print("Výsledok:", result)
+
+
+if __name__ == "__main__":
+    main()
