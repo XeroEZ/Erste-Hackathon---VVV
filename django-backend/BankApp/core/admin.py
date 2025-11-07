7daa3507ff2b3f686a8053da5979798515aea059
DolhyRichard <dolhyriso@gmail.com>
2025-11-07 07:50:39 +0100

    Clean up pycache and update .gitignore

--- django-backend/BankApp/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Shop, Receipt, Product
-
-admin.site.register(User)
-admin.site.register(Shop)
-admin.site.register(Receipt)
-admin.site.register(Product)
