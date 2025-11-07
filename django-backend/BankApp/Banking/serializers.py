06c21d9e4882fb6396f0953623dabcb44e9b000e
DolhyRichard <dolhyriso@gmail.com>
2025-11-07 07:55:22 +0100

    merge problems

---
+++ django-backend/BankApp/Banking/serializers.py
@@ -0,0 +1,13 @@
+# banking/serializers.py
+from rest_framework import serializers
+from .models import Account, Transaction
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
---
+++ django-backend/BankApp/Banking/views.py
@@ -0,0 +1,28 @@
+# banking/views.py
+
+from rest_framework import viewsets, permissions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Account, Transaction
+from .serializers import AccountSerializer, TransactionSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    @action(detail=True, methods=['get'])
+    def transactions(self, request, pk=None):
+        account = self.get_object()
+        transactions = account.transactions.all()
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data)
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+# TODO stale byt odhlaseny po ukonceni sessionu
