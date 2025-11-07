c3dd72c963b88bddce349ace2c08648e659b97b6
Martin Č <martinus5333@gmail.com>
2025-11-07 09:56:38 +0100

    +databaza

---
+++ django-backend/BankApp/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # API aplikácie
+    path('api/banking/', include('Banking.urls')),
+    path('api/core/', include('core.urls')),
+
+    # JWT tokeny
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
