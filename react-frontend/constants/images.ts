8ca34a6e0170c3c19092a99a83d6c105775a364e
Peter <peter.gramata@student.tuke.sk>
2025-11-08 06:19:51 +0100

    ikonky nove, a taby pre index, vydavky, karta, stav uctu a _layout par uprav

--- react-frontend/constants/images.ts
+++ react-frontend/constants/images.ts
@@ -3,10 +3,13 @@ import highlight from "@/assets/images/highlight.png";
 import icon from "@/assets/images/icon.png";
 import signin from "@/assets/images/signin.png";
 import welcome from "@/assets/images/welcome.png";
+import card from "@/assets/images/card.png";
+
 
 
 export const images = {
   bg,
+  card,
   highlight,
   icon,
   signin,
